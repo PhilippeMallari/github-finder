213f9193e24d9ad69f9a700b37e5b0b46d80334a
PhilippeMallari <mallari.philippe@gmail.com>
2023-04-10 16:31:10 -0600

    created get single user context and reducer | passed it to user component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Alert from "./components/layout/Alert"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
+import User from "./pages/User"
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 
@@ -25,6 +26,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
