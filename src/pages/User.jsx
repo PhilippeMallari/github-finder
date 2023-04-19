2793edc9dabb1f47453e52f1a6adaa480c9fd0ec
PhilippeMallari <mallari.philippe@gmail.com>
2023-04-19 13:41:58 -0600

    refactor code of github actions, github reducer and user.jsx | added axios | finished project

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import { useParams, Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
-import {getUser, getUserRepos} from '../context/github/GithubActions'
+import {getUserAndRepos} from '../context/github/GithubActions'
 
 export default function User() {
 
@@ -16,11 +16,8 @@ export default function User() {
   useEffect(() => {
     dispatch({type: 'SET_LOADING'})
     const getUserData = async() => {
-      const userData = await getUser(params.login)
-      dispatch({type: 'GET_USER', payload: userData})
-
-      const userRepoData = await getUserRepos(params.login)
-      dispatch({type: 'GET_REPOS', payload: userRepoData})
+      const userData = await getUserAndRepos(params.login)
+      dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
     }
 
     getUserData()
